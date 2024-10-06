09bed724a9d28cd11d5aed433047d68ec3e09c1d
johang31m <johangonzalez282971@correo.itm.edu.co>
2024-10-06 00:07:38 -0500

    interfaces

---
+++ src/app/interfaces/Reporte_trabajo.d.ts
@@ -0,0 +1,10 @@
+export interface ReporteTrabajo {
+  KioskId: string;
+  fecha: Date;
+  PictBOX: string;
+  PictBef: string;
+  PictDef: string;
+  PictAft: string;
+  nota: string;
+  name_tecnico: string;
+}
---
+++ src/app/interfaces/Sitios.d.ts
@@ -0,0 +1,10 @@
+export interface Sitios {
+  KioskId: string;
+  ParentName?: string;
+  store_id: string;
+  address: string;
+  city: string;
+  state: string;
+  zip_code: string;
+  tienda_nombre?: string;
+}
