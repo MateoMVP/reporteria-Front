26b9c48e49d8eac32ace916fc2220c3d83d3a4d2
johang31m <johangonzalez282971@correo.itm.edu.co>
2024-10-07 08:36:42 -0500

    fx

--- src/app/controllers/imageLoader.ts
+++ src/app/controllers/imageLoader.ts
@@ -1,17 +1,15 @@
 interface Props {
   src: string;
-  local:boolean
+  local?: boolean;
 }
 import AXIOS from "@/app/config/axios";
 
-export default function customImageLoader({ src,local }: Props) {
+export default function customImageLoader({ src, local }: Props) {
   try {
-     const encode = encodeURIComponent(btoa(src.trim()));
-    console.log(src)
+    const encode = encodeURIComponent(btoa(src.trim()));
     const baseURL = local
       ? "http://localhost:3005/redbox/api" // URL de localhost
       : AXIOS.defaults.baseURL; // URL pública en producción
-    console.log(baseURL)
     return baseURL + "/imagen/" + encode;
   } catch (error) {
     console.error("Error encoding image URL:", error);
