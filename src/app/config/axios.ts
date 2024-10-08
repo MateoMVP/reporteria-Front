1d649abb3840be4755f524b49817a01ba3626d4c
johang31m <johangonzalez282971@correo.itm.edu.co>
2024-10-07 23:03:14 -0500

    headers

--- src/app/config/axios.ts
+++ src/app/config/axios.ts
@@ -1,9 +1,20 @@
 import axios from "axios";
+import Cookies from "js-cookie";
+const AXIOS = axios.create({
+  baseURL: "https://6f3906702b06.sn.mynetname.net/redbox/api",
+});
 
-const AXIOS = axios.create(
-    {
-        baseURL: "https://6f3906702b06.sn.mynetname.net/redbox/api"
+AXIOS.interceptors.request.use(
+  async (config) => {
+    const token = Cookies.get("authToken");
+    if (token && config.headers) {
+      config.headers.Authorization = `Bearer ${token}`;
     }
-)
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
-export default AXIOS
+export default AXIOS;
